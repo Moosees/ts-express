53f00a936ef7d85341ff9c0ab73d025b1ecd0ddb
Moosees <linus.almgren@gmail.com>
2021-05-05 23:42:41 +0200

    setup express and login route

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log('test');
+import express from 'express';
+import { router } from './routes/loginRoutes';
+
+const app = express();
+const port = 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(router);
+
+app.listen(port, () => {
+  console.log(`Running server on port ${port}`);
+});
